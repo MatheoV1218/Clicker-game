12c17df59cbe0b33e767307dbee9f6466280ba14
MatheoV1218 <mateovillada1@outlook.com>
2025-03-05 13:19:05 -0500

    completed the upgrades and made it look better

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 let gem = document.querySelector('.gem-cost');
-let clickerCost = document.querySelector('.clicker-cost');
 let parsedGem = parseFloat(gem.innerHTML);
+
+let clickerCost = document.querySelector('.clicker-cost');
 let parsedClickerCost = parseFloat(clickerCost.innerHTML);
 let clickerLevel = document.querySelector('.clicker-level');
 let clickerIncrease = document.querySelector('.clicker-increase');
 let parsedClickerIncrease = parseFloat(clickerIncrease.innerHTML);
+
+let pickaxeCost = document.querySelector('.pickaxe-cost');
+let parsedPickaxeCost = parseFloat(pickaxeCost.innerHTML);
+let pickaxeLevel = document.querySelector('.pickaxe-level');
+let pickaxeIncrease = document.querySelector('.pickaxe-increase');
+let parsedPickaxeIncrease = parseFloat(pickaxeIncrease.innerHTML);
+
+let minerCost = document.querySelector('.miner-cost');
+let parsedMinerCost = parseFloat(minerCost.innerHTML);
+let minerLevel = document.querySelector('.miner-level');
+let minerIncrease = document.querySelector('.miner-increase');
+let parsedMinerIncrease = parseFloat(minerIncrease.innerHTML);
+let gps = 0;
 
 let gpc = 1;
 function incrementGem(){
@@ -24,3 +38,35 @@ function buyClicker() {
     clickerCost.innerHTML = Math.round(parsedClickerCost)
   }
 }
+
+function buyPickaxe() {
+  if (parsedGem >= parsedPickaxeCost) {
+    gem.innerHTML = Math.round(parsedGem -= parsedPickaxeCost);
+
+    pickaxeLevel.innerHTML ++
+
+    parsedPickaxeIncrease = parseFloat((parsedPickaxeIncrease * 1.03).toFixed(2))
+    pickaxeIncrease.innerHTML = parsedPickaxeIncrease
+    gps += parsedPickaxeIncrease
+    parsedPickaxeCost *= 1.18;
+    pickaxeCost.innerHTML = Math.round(parsedPickaxeCost)
+  }
+}
+
+function buyMiner() {
+  if (parsedGem >= parsedMinerCost) {
+    gem.innerHTML = Math.round(parsedGem -= parsedMinerCost);
+
+    minerLevel.innerHTML ++
+
+    parsedMinerIncrease = parseFloat((parsedMinerIncrease * 1.03).toFixed(2))
+    minerIncrease.innerHTML = parsedMinerIncrease
+    gps += parsedMinerIncrease
+    parsedMinerCost *= 1.18;
+    minerCost.innerHTML = Math.round(parsedMinerCost)
+  }
+}
+setInterval(() => {
+parsedGem += gps / 10;
+gem.innerHTML = Math.round(parsedGem);
+}, 100)
